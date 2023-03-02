9ba95a33a1c98d666d3434061239035898e66b31
adiyuda <adiyudapratama4@gmail.com>
2023-03-02 18:42:41 +0700

    modified manager repo and uscase

--- manager/usecase_manager.go
+++ manager/usecase_manager.go
@@ -7,6 +7,10 @@ type UsecaseManager interface {
 
 	UserUseCase() usecase.UserUseCase
 	TransactionUscase() usecase.TransactionUscase
+	MoneyChangerUsecase() usecase.MoneyChangerUsecase
+	PaymentMethodUsecase() usecase.PaymentMethodUsecase
+	TransactionTypeUsecase() usecase.TransactionTypeUsecase
+
 }
 
 type useCaseManager struct {
@@ -21,6 +25,18 @@ func (u *useCaseManager) TransactionUscase() usecase.TransactionUscase {
 
 func (u *useCaseManager) UserUseCase() usecase.UserUseCase {
 	return usecase.NewUserUseCase(u.repoManager.UserRepository())
+}
+
+func (u *useCaseManager) MoneyChangerUsecase() usecase.MoneyChangerUsecase {
+	return usecase.NewMoneyChanger(u.repoManager.MoneyChangerRepsitory())
+}
+
+func (u *useCaseManager) PaymentMethodUsecase() usecase.PaymentMethodUsecase {
+	return usecase.NewPaymentMethod(u.repoManager.PaymentMethodRepository())
+}
+
+func (u *useCaseManager) TransactionTypeUsecase() usecase.TransactionTypeUsecase {
+	return usecase.NewTransactionType(u.repoManager.TransactionTypeRepository())
 }
 
 func NewUseCaseManager(repoManager RepositoryManger) UsecaseManager {
