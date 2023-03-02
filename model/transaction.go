a0f236471b2b8a51017b3cd8e0c564477b91fd02
selfi <selfidwis58@gmail.com>
2023-03-02 09:45:29 +0700

    add folder and model file

---
+++ model/transaction.go
@@ -0,0 +1,14 @@
+package model
+
+import "time"
+
+type Transaction struct {
+	Transaction_ID      string    `json:"transaction_ID"`
+	Userwallet_id       string    `json:"userwallet_id"`
+	Money_Changer_ID    string    `json:"money_Changer_ID"`
+	Transaction_Type_ID string    `json:"transaction_Type_ID"`
+	Payment_method_id   string    `json:"payment_method_id"`
+	Amount              float64   `json:"amount"`
+	Status              bool      `json:"status"`
+	Date_Time           time.Time `json:"date_Time"`
+}
---
+++ model/user.go
@@ -0,0 +1,19 @@
+package model
+
+type User struct {
+	ID       string  `db:"userwallet_id" json:"userwallet_id"`
+	Name     string  `json:"name"`
+	Email    string  `json:"email"`
+	Phone    string  `json:"phone"`
+	Password string  `json:"password"`
+	Balance  float64 `json:"balance"`
+}
+
+type UserForm struct {
+	ID       string  `db:"userwallet_id" form:"userwallet_id"`
+	Name     string  `form:"name"`
+	Email    string  `form:"email"`
+	Phone    string  `form:"phone"`
+	Password string  `form:"password"`
+	Balance  float64 `form:"balance"`
+}
